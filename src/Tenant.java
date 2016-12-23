ff3990b4171ffbbb519ce63a885eca8b74396b03
Kunaal Bajwa <kunaal.bajwa@gmail.com>
2016-12-22 21:00:20 -0600

    up

---
+++ src/Tenant.java
@@ -0,0 +1,35 @@
+/**
+ * Created by kunaalbajwa on 12/22/16.
+ put one database
+ use intelliJ pay for it it works
+ web storm for javascript
+ use mySQL
+ look at hibernate again, give Spring a second chance
+
+ start small make one table and such; baby steps
+ */
+
+
+/*
+name, property, age, occupation, # of dependencies, pets (make thisa boolean), cars (make this an int)
+
+*/
+
+
+
+/*
+Address, notes
+
+subcat: houses					subcat: apartments
+rent							number
+sq ft							rent
+max occ							sq ft
+utilities						max occ
+#tenants						utilities
+allows pets? (boolean)			#tenants
+parking (integer)				allows pets?(boolean)
+								parking(integer)
+*/
+
+public class Tenant {
+}
